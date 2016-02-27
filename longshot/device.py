64f910cf13016351ac027e3ae4c3344703c0847d
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2016-02-27 15:51:40 +0100

    refreshed the interface

--- longshot/device.py
+++ longshot/device.py
@@ -23,19 +23,48 @@ class LongshotThread(threading.Thread):
                               'paths': all_pathpoints
                           })
 
-        if r.status_code == 200:
-            self.device.paths_synced = True
+        if r.status_code != 200:
+            raise IOError('Failed to redefine paths')
 
-            r = r.json()
+        self.device.paths_synced = True
 
-            for path, tsv in r['values'].iteritems():
-                self.device.pathpoints[path]._change(*tsv)
+        r = r.json()
+
+        # Now we need to check default values for sensors that are registered first time
+        # in this session
+
+        for path, tsv in r['values'].iteritems():
+
+            try:
+                p = self.device.pathpoints[path]
+            except KeyError:     # it might have been deleted while we were syncing!
+                continue
+
+            if p.registered:        # No need to check it.
+                continue
+
+            candidate_ts, candidate_v = tsv
+
+            did_server_value_win = False        # because if it does, we need to schedule a sync...
+            if p.value is None:     # server value automatically wins...
+                did_server_value_win = True
+            else: # there is a conflict
+                if candidate_ts > p.timestamp:
+                    did_server_value_win = True
+
+            if did_server_value_win:
+                p._change(candidate_ts, candidate_v)
+            else:
+                p._schedulesync(p.timestamp, p.value)
+
+            p.registered = True
 
     def _check_write_queue(self):
+        #todo hold writes for invalid paths
         r = requests.post(self.api_root + '/v1/get_writes/',
                           json={'device_id': self.device.device_id, 'secret': self.device.secret})
         if r.status_code != 200:
-            return
+            raise IOError('Failed to get writes')
 
         r = r.json()
         paths_updated = []
@@ -58,52 +87,61 @@ class LongshotThread(threading.Thread):
                                                                            'secret': self.device.secret,
                                                                            'paths_updated': paths_updated})
 
+
+    def _syncvalues(self):
+        # we did not break - this means there is a need to sync
+        sync_dict = {}
+
+        for pathpoint in self.device.pathpoints.values():
+            if pathpoint.values_to_store:
+                sync_dict[pathpoint.path] = sorted(pathpoint.values_to_store)
+                pathpoint.values_to_store = []
+
+        print 'syncing %s' % (sync_dict, )
+
+        r = requests.post(self.api_root + '/v1/sync_values/', json={'device_id': self.device.device_id,
+                                                                    'secret': self.device.secret,
+                                                                    'values': sync_dict})
+
+        if r.status_code != 200:
+            # If you failed syncing, return the values to pool and try another time
+            for path, values in sync_dict.iteritems():
+                try:
+                    self.device.pathpoints[path].values_to_store.extend(values)
+                except KeyError:
+                    pass
+            raise IOError('Failed to sync')
+        else:
+            for pathpoint in self.device.pathpoints.values():
+                pathpoint.synced = True
+
+
     def run(self):
         self._syncpaths()
 
         while not self.terminating:
-            # sync paths
-            if not self.device.paths_synced:
-                self._syncpaths()
+            try:
+                # sync paths
+                if not self.device.paths_synced:
+                    self._syncpaths()
 
-            # check write queue
-            self._check_write_queue()
+                # check write queue
+                self._check_write_queue()
 
-            # check if there's a need to sync any values with the server
-            need_to_sync = False
+                need_to_sync = False
+                for synced in [pathpoint.synced for pathpoint in self.device.pathpoints.values()]:
+                    if not synced:
+                        need_to_sync = True
+                        break
 
-            for pathpoint in self.device.pathpoints.values():
-                if not pathpoint.synced:
-                    need_to_sync = True
-                    break
+                if need_to_sync:
+                    self._syncvalues()
 
-            if need_to_sync:
-                # sync'em
+                self.device.persistence.sync()
+                time.sleep(60)
 
-                sync_dict = {}
-
-                for pathpoint in self.device.pathpoints.values():
-                    if pathpoint.values_to_store:
-                        sync_dict[pathpoint.path] = sorted(pathpoint.values_to_store)
-                        pathpoint.values_to_store = []
-
-                r = requests.post(self.api_root + '/v1/sync_values/', json={'device_id': self.device.device_id,
-                                                                            'secret': self.device.secret,
-                                                                            'values': sync_dict})
-
-                if r.status_code != 200:
-                    # If you failed syncing, return the values to pool and try another time
-                    for path, values in sync_dict.iteritems():
-                        try:
-                            self.device.pathpoints[path].values_to_store.extend(values)
-                        except KeyError:
-                            pass
-                else:
-                    for pathpoint in self.device.pathpoints.values():
-                        pathpoint.synced = True
-
-            self.device.persistence.sync()
-            time.sleep(60)
+            except IOError:
+                pass
 
 
 class LongshotDevice(object):
@@ -122,7 +160,7 @@ class LongshotDevice(object):
         self.device_id = device_id
         self.secret = secret
         self.pathpoints = {}  # path name => LongshotPathpoint
-        self.done_registering = False       #: was first /v1/redefine_paths/ called?
+        self.done_registering = False       #: was .done() called?
         self.paths_synced = False           #: is there a need to synchronize patches?
         self.thread = LongshotThread(self, longshot_path)
 
@@ -156,33 +194,44 @@ class LongshotDevice(object):
         """
         return self.pathpoints[path]    # raises KeyError
 
-    def register(self, path, default=None):
-        """Create and register new pathpoint
-
-            p = LongshotPathpoint(path, device)
-
-        is equivalent to
-
-            p = device.register(path)
+    def register(self, path, default_value=None, default_timestamp=None):
+        """
+        Create and register new pathpoint.
 
         :param path: path name
+
+        :param value: a candidate for pathpoint's value. This will be resolved against PersistenceLayer
+        :param timestamp: timestamp to use as candidate. Current timestamp if not None.
+
         :return: LongshotPathpoint to interface with the pathpoint
+        :raises ValueError: pathpoint is currently registered!
         """
 
+        default_timestamp = default_timestamp or time.time()
+
         if path in self.pathpoints:
-            return self.pathpoints[path]
+            raise ValueError('Pathpoint exists!')
+
+        pp = LongshotPathpoint(path, self)
+
+        # Obtain the default value - read persistence
+        try:
+            timestamp, value = self.persistence.get_current_value(path)
+        except TypeError:   # None can't be unpacked. Use provided defaults
+            if default_value is not None:
+                pp.value = default_value
+                pp.timestamp = default_timestamp
+            # else, they just stay None. There is no value...
         else:
-            pp = LongshotPathpoint(path, self)
+            # We have read persistence successfully. Try to anlyze provided default.
+            if default_value is not None:
+                if pp.timestamp < default_timestamp:        # default wins
+                    pp.value = default_value
+                    pp.timestamp = default_timestamp
 
-            try:
-                timestamp, value = self.persistence.get_value(path)
-            except TypeError:   # None can't be unpacked
-                pp.value = default
-                pp.timestamp = None
-
-            self.paths_synced = False
-            self.pathpoints[path] = pp
-            return pp
+        self.paths_synced = False
+        self.pathpoints[path] = pp
+        return pp
 
     def done(self):
         """
@@ -195,10 +244,11 @@ class LongshotDevice(object):
 
         After this, further pathpoints can be registered/unregistered via method calls, and they will be
         synced too
+
+        :raises RuntimeError: called more than twice
         """
         if self.done_registering:
-            warnings.warn(RuntimeWarning('done() called twice or more'))
-            return  # this cannot be called twice
+            raise RuntimeError('called .done() twice')
 
         self.done_registering = True
         self.thread.start()
@@ -214,71 +264,50 @@ class LongshotPathpoint(object):
     A single pathpoint, as registered in Longshot system
     """
 
-    def __init__(self, path, device=None):
+    def __init__(self, path, device):
         """
         :type path: str or unicode
-        :param device: pass a LongshotDevice if this should be registered immediately. Pass None
-            if call to .register() is needed
+        :param device: attached device
         :type device: LongshotDevice
         """
         self.device = device
         self.path = path
 
-        self.values_to_store = []  # list of timestamp, value
+        self.values_to_store = []  # list of (timestamp, value) - archival value to store
         self.listeners = []  # callable/1 to be invoked with new value upon change
 
-        self.value = None  # current value
+        # Current value - timestamp, value
+        self.value = None
         self.timestamp = None
-        self.synced = False
-        self.registered = False
 
-    def unregister(self):
-        self.device.unregister(self.path)
+        self.synced = False          # Are there any new values that need to be sent to server?
+        self.registered = False      # Has this been declared on the remote server?
 
-    def register(self, device):
-        if self.registered:
-            raise RuntimeError('Already registered')
-
-        self.device = device
-
-        if self.path in self.device.pathpoints:
-            raise RuntimeError('Already registered')
-
-        self.device.register(self)
-        self.registered = True
+        # default value will be loaded just now by LongshotDevice.register() that called this constructor
 
     def listen(self, callable):
         """
         Register callable to be called if value of this pathpoint changes.
-        Two sources of these changes are possible:
-            - listeners are called with updated value after registering this pathpoint
-            - server issues us a change command
-        Listener will not be called on updating the value by .store()
+        Following sources of these changes are possible:
+            - server has a more recent value than we do (we are just declaring this path)
+            - server issues us a WRITE command
+            - internal change via .update()
 
-        Listener can be invoked from either the thread that launches .done() or from internal Longshot thread.
+        Listener can be invoked from either the thread that launches .store() or from internal Longshot thread.
         Take care with concurrency.
+        :return: self
         """
         self.listeners.append(callable)
-
-
-    def push(self):
-        """
-        a store() with current values. No effect is value empty
-
-        Returns self
-        """
-        if self.value is None:
-            return self
-
-        self.store(self.value, self.timestamp)
         return self
+
 
     def update(self, value, timestamp=None):
         """
-        Value of the register has changed.
+        Value of the register has changed due to application-internal reasons.
 
         This is meant to be called by other part inside this application, but other than the one
-        that registered the pathpoint and is responsible for this.
+        that registered the pathpoint and is responsible for this. Because of this, it can be used
+        to implement a sort of intra-process communication.
 
         This will schedule a sync with the server, and call listeners - just as it a new value
         was received from the server.
@@ -289,12 +318,19 @@ class LongshotPathpoint(object):
         """
         self._change(timestamp or time.time(), value)
 
+    def _schedulesync(self, timestamp, value):
+        """
+        Make timestamp and value sync to server at later time
+        """
+        self.values_to_store.append((int(timestamp*1000), value))    # because server deals in ms
+        self.synced = False
+
     def _change(self, timestamp, value):
         """
-        This will JUST set current value and call listeners.
-
-        If value is the same, nothing will change
-        If timestamp is less, nothing will change"""
+        Set the current value and call the listeners. DO NOT schedule a server update
+        :param timestamp: timestamp of the update. Nothing will happen if this is less
+            than current timestamp
+        """
 
         if self.timestamp > timestamp:
             return
@@ -305,22 +341,22 @@ class LongshotPathpoint(object):
         for listener in self.listeners:
             listener(self.value)
 
-        self.device.persistence.set_value(self.path, value)
+        self.device.persistence.set_current_value(self.path, value)
 
     def store(self, value, timestamp=None):
         """
         Update this pathpoint with a new value.
 
-        This is meant to be called by the application that registered the point itself,
+        This is meant to be called by the part of application that registered the point itself,
         means it knows about the update.
 
-        This will NOT invoke all listeners on this path
+        This will NOT invoke listeners on this path
 
         :param timestamp: UNIX timestamp or None for current system time
         """
-        timestamp = int(timestamp or time.time())
+        timestamp = timestamp or time.time()
 
-        self.values_to_store.append((timestamp*1000, value))    # because server deals in ms
-        self.device.persistence.set_value(self.path, value)
+        self._schedulesync(timestamp, value)
 
-        self.synced = False
+        self.device.persistence.set_current_value(self.path, value)
+
--- longshot/persistence/__init__.py
+++ longshot/persistence/__init__.py
@@ -2,20 +2,39 @@ import shelve
 import time
 
 class NoPersistenceLayer(object):
+    """
+    This is a base persistence layer for storing data. It stores current value of a sensor,
+    and its historic value (in case net is broken and we need to restore later
+    """
     def __init__(self):
         pass
 
-    def get_value(self, path):
-        """Return timestamp, value or None"""
+    def get_current_value(self, path):
+        """
+        Obtain current value of a sensor
+        :param path: path of the value
+        """
         return None
 
-    def set_value(self, path, value, timestamp=None):
+    def set_current_value(self, path, value, timestamp=None):
+        """
+        Set the value of a sensor.
+        :param path: path of the sensor
+        :param value: sensor's value
+        :param timestamp: timestamp of the write. If None, current will be used.
+        :return:
+        """
         pass
 
     def sync(self):
+        """Synchronize to disk"""
         pass
 
-    def del_value(self):
+    def del_current_value(self, path):
+        """
+        Delete a sensor - it's current value and historic
+        :param path: sensor path
+        """
         pass
 
 
@@ -24,19 +43,36 @@ class ShelfPersistenceLayer(object):
         self.shelf = shelve.open(str(filename), protocol=-1)
 
     def sync(self):
+        """Synchronize to disk"""
         self.shelf.sync()
 
-    def del_value(self, path):
-        try:
-            del self.shelf[path]
-        except KeyError:
-            pass
-
-    def get_value(self, path):
+    def get_current_value(self, path):
+        """
+        Obtain current value of a sensor
+        :param path: path of the value
+        :return: timestamp, value
+        """
         try:
             return self.shelf[path]
         except KeyError:
             return None
 
-    def set_value(self, path, value, timestamp=None):
+    def set_current_value(self, path, value, timestamp=None):
+        """
+        Set the value of a sensor.
+        :param path: path of the sensor
+        :param value: sensor's value
+        :param timestamp: timestamp of the write. If None, current will be used.
+        :return:
+        """
         self.shelf[path] = timestamp or time.time(), value
+
+    def del_current_value(self, path):
+        """
+        Delete a sensor - it's current value and historic
+        :param path: sensor path
+        """
+        try:
+            del self.shelf[path]
+        except KeyError:
+            pass
